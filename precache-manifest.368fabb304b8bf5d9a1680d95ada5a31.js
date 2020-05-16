5a133429bd7e3b8441e643ddf3a85985956498b0
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-16 07:57:19 +0200

    Updates

---
+++ precache-manifest.368fabb304b8bf5d9a1680d95ada5a31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fff465212f242fcc2e3af2020c092e2",
+    "url": "/Teachnical-Assignment/index.html"
+  },
+  {
+    "revision": "f8a4e5a0d596fc1dd931",
+    "url": "/Teachnical-Assignment/static/css/main.23f047dd.chunk.css"
+  },
+  {
+    "revision": "98e847915767c661c6a5",
+    "url": "/Teachnical-Assignment/static/js/2.2e5307f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Teachnical-Assignment/static/js/2.2e5307f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a4e5a0d596fc1dd931",
+    "url": "/Teachnical-Assignment/static/js/main.1ad4f66a.chunk.js"
+  },
+  {
+    "revision": "db852815be1944b95ef1",
+    "url": "/Teachnical-Assignment/static/js/runtime-main.c077464f.js"
+  },
+  {
+    "revision": "58764f005cddf5fd3367354439bdf0db",
+    "url": "/Teachnical-Assignment/static/media/background.58764f00.jpg"
+  }
+]);
